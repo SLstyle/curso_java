47baf33bd3747a0c034f9c83c3065e2928395733
yaguin <yagofagundes0@gmail.com>
2025-03-31 21:53:14 -0300

    contenacao

---
+++ src/PrimeiroProjeto.java
@@ -0,0 +1,13 @@
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+
+        int idade = 31;
+        String nome = "maria";
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+
+    }
+
+
+}

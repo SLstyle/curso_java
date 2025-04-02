7fd91c546bd878925e301d51e35857b3065abc07
yaguin <yagofagundes0@gmail.com>
2025-04-02 20:32:46 -0300

    contenacao

--- src/PrimeiraAtividade.java
+++ src/PrimeiraAtividade.java
@@ -10,7 +10,7 @@ public class PrimeiraAtividade {
         double price1 = 2100.0;
         double price2 = 650.50;
         double measure = 53.234567;
-        System.out.println("Products:");
+        System.out.println("Products:"
         System.out.printf("%s, whict price is $ %f %n", product1, price1);
         System.out.printf("%s, whict price is $ %f %n%n", product2, price2);
         System.out.printf("Record: %d years old, code %d and gender: %s %n%n", age, code, gender);
